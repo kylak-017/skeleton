479494402a61270b13170b49ca69dc9d0ccefac0
Kyla Kim <KyuriKim017@gmail.com>
2024-03-04 02:06:28 +0900

    changes

--- client/src/components/modules/SingleStory.js
+++ client/src/components/modules/SingleStory.js
@@ -2,23 +2,23 @@ import React from "react";
 import { Link } from "@reach/router";
 
 /**
- * Component to render a single comment
+ * Story is a component that renders creator and content of a story
  *
  * Proptypes
- * @param {string} _id of comment
+ * @param {string} _id of the story
  * @param {string} creator_name
  * @param {string} creator_id
- * @param {string} content of the comment
+ * @param {string} content of the story
  */
-const SingleComment = (props) => {
+const SingleStory = (props) => {
   return (
-    <div className="Card-commentBody">
+    <div className="Card-story">
       <Link to={`/profile/${props.creator_id}`} className="u-link u-bold">
         {props.creator_name}
       </Link>
-      <span>{" | " + props.content}</span>
+      <p className="Card-storyContent">{props.content}</p>
     </div>
   );
 };
 
-export default SingleComment;
+export default SingleStory;
